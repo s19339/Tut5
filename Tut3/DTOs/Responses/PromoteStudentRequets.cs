2f90ee01e19bfa963e770e47b86f6bdeb0dbe937
ASUS <ASUS@INTEL>
2020-04-12 21:06:25 +0200

    half done

---
+++ Tut3/DTOs/Responses/PromoteStudentRequets.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tut3.DTOs.Responses
+{
+    public class PromoteStudentRequets
+    {
+    }
+}
